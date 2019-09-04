57d3dffdf25d8b82011de5a131671e89bf7f9392
shubhamyadav20 <shubhamyadav.sy544@gmail.com>
2019-09-04 04:55:57 -0700

    v1

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { AppComponent } from './app.component';
+import { ProductFilterPipe } from './ProductFilterPipe.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,ProductFilterPipe
+  ],
+  imports: [
+    BrowserModule,FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
